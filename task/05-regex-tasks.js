5f3bdf16b9ceca7bc135ad84eebf92fdfc7fab7c
Arkady Polhovsky <arkims@inbox.ru>
2020-07-04 21:17:27 +0300

    task 5

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -31,7 +31,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-   throw new Error('Not implemented');
+   return /^\{[\da-f]{8}(-[\da-f]{4}){3}-[\da-f]{12}\}$/i
 }
 
 
@@ -53,7 +53,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-   throw new Error('Not implemented');
+   return /p.[^ar]/
 }
 
 
@@ -72,7 +72,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+   return /^([012]?\d?\d)(\.[012]?\d?\d){3}$/
 }
 
 
@@ -91,7 +91,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-   throw new Error('Not implemented');
+   return /\d{3}(?<!0{3})-(?!0{2})\d{2}-(?!0{4})\d{4}/
 }
 
 
@@ -116,7 +116,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   throw new Error('Not implemented');
+   return new RegExp(`^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{${minLength},}$`);
 }
 
 
